2ff05c4a44ee25d36b252079ee0425d858fd2bfc
Your Name <you@example.com>
2022-01-27 18:18:21 +0100

    правки после ревью 25.01

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,15 @@
 let popup = document.querySelector('.popup')
 let editButtton = document.querySelector('.profile__edit-btn');
-let saveButton = document.querySelector('.popup__btn');
 let closeButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form') 
 let nameInput = formElement.querySelector('.popup__input_type_name') 
 let jobInput = formElement.querySelector('.popup__input_type_about')
 let profileTitle = document.querySelector('.profile__title');
-let profileSubTitle = document.querySelector('.profile__subtitle');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function popupOpen() {
     nameInput.value = profileTitle.textContent;
-    jobInput.value = profileSubTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
     popup.classList.add('popup_open');
 }
 
@@ -21,8 +20,7 @@ function popupClose() {
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
-    profileSubTitle.textContent = jobInput.value;
-    popupOpen();
+    profileSubtitle.textContent = jobInput.value;
     popupClose();
 }
 

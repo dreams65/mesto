b1de84000ccbed82874529da1b685a099387b3dd
Your Name <you@example.com>
2022-01-30 16:21:13 +0100

    добавлены именения в main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
 let popup = document.querySelector('.popup')
 let editButtton = document.querySelector('.profile__edit-btn');
 let closeButton = document.querySelector('.popup__close-button');
@@ -6,6 +33,28 @@ let nameInput = formElement.querySelector('.popup__input_type_name')
 let jobInput = formElement.querySelector('.popup__input_type_about')
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+
+//добавления карточек
+let addImageBtn = document.querySelector('.profile__add-btn');
+let cardContainer = document.querySelector('.elements__grid-container');
+let userCard = document.querySelector('.user-card').content;
+
+initialCards.forEach(item => {
+    renderCards(item.name, item.link);
+    cardContainer.append(item);
+});
+
+function renderCards(name, link) {
+    const newCard = userCard.cloneNode(true);
+    newCard.querySelector('.card__title').textContent = name;
+    newCard.querySelector('.card__image').src = link;
+    cardContainer.prepend(newCard);
+}
+
+
+
+
+
 
 function popupOpen() {
     nameInput.value = profileTitle.textContent;
